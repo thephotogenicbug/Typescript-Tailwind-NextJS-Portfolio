4bab49a2b5224bb769f6a2e7327fe571cc13ca2a
thephotogenicbug <navinkumarachari@gmail.com>
2022-10-28 13:50:47 +0530

    Design HeroSection 2/2

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import HeroImg from "./hero_img.jpg";
+import Image from "next/image";
+import Link from "next/link";
 type Props = {};
 
 export default function Hero({}: Props) {
@@ -17,11 +20,34 @@ export default function Hero({}: Props) {
   return (
     <div className=" h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden ">
       <BackgroundCircles />
-      
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#f7a00a" />
-      </h1>
+      <Image
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src={HeroImg}
+        alt=""
+      />
+      <div className="  z-20 ">
+        <h2 className=" text-sm uppercase text-gray-500 pb-2 tracking-[15px] ">
+          Web Developer
+        </h2>
+        <h1 className=" text-5xl lg:text-6xl font-semibold px-10 ">
+          <span className=" mr-3 ">{text}</span>
+          <Cursor cursorColor="#f7a00a" />
+        </h1>
+        <div className=" pt-5 ">
+          <Link href="#experience">
+            <button className=" heroButton ">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className=" heroButton ">Skills</button>
+          </Link>
+          <Link href="#about">
+            <button className=" heroButton ">About</button>
+          </Link>
+          <Link href="#projects">
+            <button className=" heroButton ">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
